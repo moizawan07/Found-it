dfdfa4681df31dd34f0174b8951780f9fc3b3d0d
moizawan07 <moizahmedawan321@gmail.com>
2024-10-02 21:34:36 -0700

    Found it

---
+++ app.js
@@ -0,0 +1,20 @@
+var flag = "true"
+
+function task(){
+    
+        var inputGet = document.getElementById("inputText").value;  //  GET HTML INPUT AND PARAGRAPHY BT GIVEN A  "ID"
+        var paraGet = document.getElementById("paragraph").innerText;
+    
+            for(var i =0; i < paraGet.length;i++){
+        
+              if(inputGet == paraGet.slice(i, i+inputGet.length)){
+                alert(inputGet + " " + "found")
+                flag = "false"
+            }
+          }
+          if(flag === "true"){
+            alert("Not found")
+          }
+        }
+        
+        
